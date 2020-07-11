803e6def7d33a6aef6ceeb8ed8abd30ad0526480
himanshuvashist <35025815+himanshuvashist@users.noreply.github.com>
2020-07-11 08:13:03 +0530

    restructure component directories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Dashboard from './Dashboard'
+import Dashboard from './components/Dashboard'
 export default class App extends Component {
     render() {
         return (
---
+++ src/components/LibraryPage/LibraryPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class LibraryPage extends Component {
+    render() {
+        return <div>here are all the saved stuff</div>
+    }
+}
